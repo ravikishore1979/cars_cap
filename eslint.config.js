a2120e617604567ed88d81cc16074606632d2e94
Ravi Kishore K <ravikishore1979@gmail.com>
2026-03-15 22:42:05 +0530

    modified esling config

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    languageOptions: {
+      globals: {
+        browser: true,
+        node: true,
+      },
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "error",
+    },
+  },
+];
